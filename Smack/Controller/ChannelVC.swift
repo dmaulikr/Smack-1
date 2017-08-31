316ae449fc0aa9f1d2b55498c90ff24c9745de2b
Virgil Nilson <virgilnilson@gmail.com>
2017-08-31 11:11:49 -0700

    End of Lesson 2

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Virgil Nilson on 8/31/17.
+//  Copyright © 2017 Virgil Nilson. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+}
