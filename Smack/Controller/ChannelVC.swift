cb54144d4183c3a3af92157b143043f00c71111f
Virgil Nilson <virgilnilson@gmail.com>
2017-09-01 12:38:09 -0700

    lesson 18

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -30,7 +30,15 @@ class ChannelVC: UIViewController {
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
     
+    override func viewDidAppear(_ animated: Bool) {
+        setupUserInfo()
+    }
+    
     @objc func userDataDidChange(_ notif: Notification) {
+        setupUserInfo()
+    }
+    
+    func setupUserInfo() {
         if AuthService.instance.isLoggedIn {
             loginBtn.setTitle(UserDataService.instance.name, for: .normal)
             userImg.image = UIImage(named: UserDataService.instance.avatarName)
--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var invalidTxt: UILabel!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passText: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
         // Do any additional setup after loading the view.
     }
     
@@ -22,6 +27,40 @@ class LoginVC: UIViewController {
     
     @IBAction func createAccountBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        self.invalidTxt.isHidden = true
+        
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passText.text , passText.text != "" else { return }
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    } 
+                })
+            } else {
+                self.spinner.isHidden = true
+                self.spinner.stopAnimating()
+                self.invalidTxt.isHidden = false
+            }
+        }
+    }
+    
+    func setupView() {
+        spinner.isHidden = true
+        invalidTxt.isHidden = true
+        emailTxt.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedStringKey.foregroundColor: SMACKPURPLEPLACEHOLDER])
+        passText.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: SMACKPURPLEPLACEHOLDER])
+
     }
     
 
