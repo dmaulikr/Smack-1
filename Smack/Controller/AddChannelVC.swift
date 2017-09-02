a892bdcca0b44bf0d21ae021c9ed273c325362db
Virgil Nilson <virgilnilson@gmail.com>
2017-09-01 22:58:39 -0700

    lesson 21

---
+++ Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,41 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Virgil Nilson on 9/1/17.
+//  Copyright © 2017 Virgil Nilson. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var descriptionTxt: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor: SMACKPURPLEPLACEHOLDER])
+        descriptionTxt.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor: SMACKPURPLEPLACEHOLDER])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -70,5 +70,11 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return MessageService.instance.channels.count
     }
-
+    
+    @IBAction func addChannelPressed(_ sender: Any) {
+        let addChannel = AddChannelVC()
+        addChannel.modalPresentationStyle = .custom
+        present(addChannel, animated: true, completion: nil)
+    }
+    
 }
